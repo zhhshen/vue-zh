e4669da6568f22eda2b1d68ece70c526c9d1f01f
shen <shen@shendeMacBook-Pro.local>
2018-01-22 17:45:30 +0800

    fix table

--- src/components/Form/FormOrganization.vue
+++ src/components/Form/FormOrganization.vue
@@ -1,9 +1,11 @@
 <template lang="html">
-  <div class="ms-organization-wrap" :style="customStyle" v-if="organization.length">
+<div class="ms-organization-wrap"
+    v-if="organization.length"
+    :style="customStyle" >
     <div class="ms-organization-title" @click="toggleChild">
-      <span v-show="!selectedItems.length || multipleSwitch">{{hint}}</span>
-      <span v-show="(selectedItems.length >0 && !multipleSwitch)">{{selectedItems | filterContent}}</span>
-      <i class="ion-chevron-down ms-organization-icon"></i>
+        <span v-show="!selectedItems.length || multipleSwitch">{{ hint }}</span>
+        <span v-show="(selectedItems.length >0 && !multipleSwitch)">{{selectedItems | filterContent}}</span>
+        <i class="ion-chevron-down ms-organization-icon"></i>
     </div>
     <div class="ms-organization-body" v-show="visible" :style="customStyle">
       <div  class="ms-organization-search">
@@ -27,290 +29,302 @@
 
 <script>
 export default {
-  name: 'form-organization',
-  props: {
-    organization: {
-      type: Array,
-      default: function () {
-        return []
-      }
+    name: 'form-organization',
+    props: {
+        organization: {
+            type: Array,
+            default: () => []
+        },
+
+        hint: {
+            type: String,
+            default: '请选择'
+        },
+
+        value: {
+            required: false
+        },
+
+        placeholder: {
+            type: String,
+            default: '搜索'
+        },
+
+        customStyle: {
+            type: Object,
+            default: () => {}
+        }
     },
-    hint: {
-      type: String,
-      default: '请选择'
+    data () {
+        return {
+            organizationNavs: [],
+            currParent: null,
+            currOrganization: this.organization,
+            content: [],
+            result: [],
+            visible: false,
+            cur: '',
+            searchVal: '',
+            inputValue: null,
+            multipleSwitch: false
+        }
     },
-    value: {
-      required: false
+    watch: {
+        value(newVal) {
+            this.inputValue = newVal
+        },
+        inputValue(newVal) {
+            this.$emit('input', newVal)
+        }
     },
-    placeholder: {
-      type: String,
-      default: '搜索'
+    created() {
+        this.currOrganization = this.organization
+        this.inputValue = this.value
     },
-    customStyle: {
-      type: Object,
-      default: () => {}
-    }
-  },
-  data () {
-    return {
-      organizationNavs: [],
-      currParent: null,
-      currOrganization: this.organization,
-      content: [],
-      result: [],
-      visible: false,
-      cur: '',
-      searchVal: '',
-      inputValue: null,
-      multipleSwitch: false
-    }
-  },
-  watch: {
-    value (newVal) {
-      this.inputValue = newVal
-    },
-    inputValue (newVal) {
-      this.$emit('input', newVal)
-    }
-  },
-  created () {
-    this.currOrganization = this.organization
-    this.inputValue = this.value
-  },
-  computed: {
-    filterSearch () {
-      var newVal = this.searchVal
-      if (!newVal) {
-        return this.currOrganization.slice(0)
-      } else {
-        return this.currOrganization.filter(item => {
-          return (item.value.indexOf(newVal) !== -1 || item.label.indexOf(newVal) !== -1)
-        })
-      }
-    },
-    selectedItems () {
-      this.cur = ''
-      this.content = []
-      let seletedValues = []
-      if (Array.isArray(this.inputValue)) {
-        seletedValues = this.inputValue
-      } else if (this.inputValue !== null) {
-        seletedValues.push(this.inputValue)
-      } else {
-        return []
-      }
-      this.currOrganization = this.organization
-      let output = {}
-      function filterArray (data, name) {
-        for (var i = 0; i < data.length; i++) {
-          var obj = data[i]
-          if (!obj || !obj.value) {
-            continue
-          }
-          if (obj.value === name) {
-            output = {
-              parentNode: data,
-              node: data[i]
+    computed: {
+        filterSearch() {
+            var newVal = this.searchVal
+            if (!newVal) {
+                return this.currOrganization.slice(0)
+            } else {
+                return this.currOrganization.filter(item => {
+                    return (
+                        item.value.indexOf(newVal) !== -1 ||
+                        item.label.indexOf(newVal) !== -1
+                    )
+                })
             }
-            break
-          } else if (obj.children && obj.children.length) {
-            filterArray(obj.children, name)
-          } else {
-          }
+        },
+        selectedItems() {
+            this.cur = ''
+            this.content = []
+            let seletedValues = []
+            if (Array.isArray(this.inputValue)) {
+                seletedValues = this.inputValue
+            } else if (this.inputValue !== null) {
+                seletedValues.push(this.inputValue)
+            } else {
+                return []
+            }
+            this.currOrganization = this.organization
+            let output = {}
+            function filterArray(data, name) {
+                for (var i = 0; i < data.length; i++) {
+                    var obj = data[i]
+                    if (!obj || !obj.value) {
+                        continue
+                    }
+                    if (obj.value === name) {
+                        output = {
+                            parentNode: data,
+                            node: data[i]
+                        }
+                        break
+                    } else if (obj.children && obj.children.length) {
+                        filterArray(obj.children, name)
+                    } else {
+                    }
+                }
+                return output
+            }
+            this.organizationNavs = []
+            for (var i = 0; i < seletedValues.length; i++) {
+                var parent = filterArray(
+                    this.currOrganization,
+                    seletedValues[i]
+                )
+                var node = parent.node
+                if (node) {
+                    if (node.children && node.children.length) {
+                        this.organizationNavs.push({
+                            value: node.value,
+                            label: node.label,
+                            children: node.children,
+                            parentNode: parent.parentNode
+                        })
+                        this.currOrganization = node.children
+                    } else {
+                        this.currOrganization = parent.parentNode
+                        this.cur = node.value
+                    }
+                    this.content.push({
+                        value: node.value,
+                        label: node.label
+                    })
+                }
+            }
+            return this.content
         }
-        return output
-      }
-      this.organizationNavs = []
-      for (var i = 0; i < seletedValues.length; i++) {
-        var parent = filterArray(this.currOrganization, seletedValues[i])
-        var node = parent.node
-        if (node) {
-          if (node.children && node.children.length) {
-            this.organizationNavs.push({
-              value: node.value,
-              label: node.label,
-              children: node.children,
-              parentNode: parent.parentNode
-            })
-            this.currOrganization = node.children
-          } else {
-            this.currOrganization = parent.parentNode
-            this.cur = node.value
-          }
-          this.content.push({
-            value: node.value,
-            label: node.label
-          })
+    },
+
+    methods: {
+        toggleChild: function() {
+            this.visible ? this.hide() : this.show()
+        },
+        show() {
+            this.visible = true
+            document.addEventListener('click', this.closeOnDocumentClick, false)
+        },
+
+        hide() {
+            this.visible = false
+            document.removeEventListener(
+                'click',
+                this.closeOnDocumentClick,
+                false
+            )
+        },
+
+        closeOnDocumentClick(e) {
+            if (!this.$el.contains(e.target)) {
+                this.hide()
+            }
+        },
+        showChild(item) {
+            let organizationNavs = this.organizationNavs
+            let currOrganization = this.currOrganization
+            this.searchVal = ''
+            if (item.children && item.children.length) {
+                organizationNavs.push({
+                    value: item.value,
+                    label: item.label,
+                    children: item.children,
+                    parentNode: currOrganization
+                })
+                this.content.push({
+                    value: item.value,
+                    label: item.label
+                })
+                this.organizationNavs = organizationNavs
+                this.currOrganization = item.children
+                this.multipleSwitch = true
+            } else if (this.content.indexOf(item) === -1) {
+                if (this.cur) {
+                    this.content.pop()
+                }
+                this.content.push({
+                    value: item.value,
+                    label: item.label
+                })
+                this.cur = item.value
+                this.visible = false
+                if (this.content.length === 1) {
+                    this.inputValue = this.content[0].value
+                    return
+                }
+                this.multipleSwitch = false
+            }
+            this.inputValue = this.content.map(i => i.value)
+        },
+        changeNav(nav, inx) {
+            let organizationNavs = this.organizationNavs
+            let content = this.content
+            this.currOrganization = nav.parentNode
+            if (inx === 0) {
+                this.content = []
+                this.organizationNavs = []
+            } else {
+                this.organizationNavs = organizationNavs.splice(0, inx)
+                this.content = content.splice(0, inx)
+            }
+            this.cur = ''
+            this.multipleSwitch = true
+            this.inputValue = this.content.map(i => i.value)
         }
-      }
-      return this.content
-    }
-  },
-
-  methods: {
-    toggleChild: function () {
-      this.visible ? this.hide() : this.show()
     },
-    show () {
-      this.visible = true
-      document.addEventListener('click', this.closeOnDocumentClick, false)
+    filters: {
+        filterContent: function(content) {
+            return content.length ? content.map(i => i.label).join('/') : ''
+        }
     },
-
-    hide () {
-      this.visible = false
-      document.removeEventListener('click', this.closeOnDocumentClick, false)
-    },
-
-    closeOnDocumentClick (e) {
-      if (!this.$el.contains(e.target)) {
+    beforeDestroy() {
         this.hide()
-      }
-    },
-    showChild (item) {
-      let organizationNavs = this.organizationNavs
-      let currOrganization = this.currOrganization
-      this.searchVal = ''
-      if (item.children && item.children.length) {
-        organizationNavs.push({
-          value: item.value,
-          label: item.label,
-          children: item.children,
-          parentNode: currOrganization
-        })
-        this.content.push({
-          value: item.value,
-          label: item.label
-        })
-        this.organizationNavs = organizationNavs
-        this.currOrganization = item.children
-        this.multipleSwitch = true
-      } else if (this.content.indexOf(item) === -1) {
-        if (this.cur) {
-          this.content.pop()
-        }
-        this.content.push({
-          value: item.value,
-          label: item.label
-        })
-        this.cur = item.value
-        this.visible = false
-        if (this.content.length === 1) {
-          this.inputValue = this.content[0].value
-          return
-        }
-        this.multipleSwitch = false
-      }
-      this.inputValue = this.content.map(i => i.value)
-    },
-    changeNav (nav, inx) {
-      let organizationNavs = this.organizationNavs
-      let content = this.content
-      this.currOrganization = nav.parentNode
-      if (inx === 0) {
-        this.content = []
-        this.organizationNavs = []
-      } else {
-        this.organizationNavs = organizationNavs.splice(0, inx)
-        this.content = content.splice(0, inx)
-      }
-      this.cur = ''
-      this.multipleSwitch = true
-      this.inputValue = this.content.map(i => i.value)
     }
-  },
-  filters: {
-    filterContent: function (content) {
-      return content.length ? content.map(i => i.label).join('/') : ''
-    }
-  },
-  beforeDestroy () {
-    this.hide()
-  }
 }
 </script>
 
 <style lang="less">
 /*级联选择器*/
 .ms-organization-wrap {
-  display: inline-flex;
-  position: relative;
-  width: 300px;
+    display: inline-flex;
+    position: relative;
+    width: 300px;
 }
-.ms-organization-title{
-  position: relative;
-  width: 100%;
-  height: 38px;
-  line-height: 28px;
-  padding: 5px 10px;
-  border-bottom: 1px solid rgba(81,130,228,.5);
+.ms-organization-title {
+    position: relative;
+    width: 100%;
+    height: 38px;
+    line-height: 28px;
+    padding: 5px 10px;
+    border-bottom: 1px solid rgba(81, 130, 228, 0.5);
 }
 .ms-organization-icon {
-  position: absolute;
-  display: inline-block;
-  top: 10px;
-  right: 0px;
-  font-size: 10px;
-  color: #999;
+    position: absolute;
+    display: inline-block;
+    top: 10px;
+    right: 0px;
+    font-size: 10px;
+    color: #999;
 }
 .ms-organization-body {
-  position: absolute;
-  top: 38px;
-  left: 0px;
-  z-index: 100;
-  width: 300px;
-  height: auto;
-  min-height: 100px;
-  max-height: 300px;
-  background: #fff;
-  border-radius: 3px;
-  overflow: hidden;
-  box-shadow: 0 0 6px 0 rgba(0,0,0,.1), 0 0 12px 5px rgba(0,0,0,.1);
+    position: absolute;
+    top: 38px;
+    left: 0px;
+    z-index: 100;
+    width: 300px;
+    height: auto;
+    min-height: 100px;
+    max-height: 300px;
+    background: #fff;
+    border-radius: 3px;
+    overflow: hidden;
+    box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.1), 0 0 12px 5px rgba(0, 0, 0, 0.1);
 }
 .ms-organization-search {
-  padding: 10px;
+    padding: 10px;
 }
 .ms-organization-search input {
-  width: 100%;
-  height: 34px;
-  padding: 5px;
-  border: none;
-  border-bottom: 1px solid rgba(81,130,228,.5);
-  outline: none;
+    width: 100%;
+    height: 34px;
+    padding: 5px;
+    border: none;
+    border-bottom: 1px solid rgba(81, 130, 228, 0.5);
+    outline: none;
 }
 .ms-organization-router {
-  line-height: 20px;
-  padding: 5px 10px;
-  width: 100%;
-  overflow-x: scroll;
+    line-height: 20px;
+    padding: 5px 10px;
+    width: 100%;
+    overflow-x: scroll;
 }
 .ms-organization-router a {
-  cursor: pointer;
+    cursor: pointer;
 }
 .ms-organization-body-top {
-  bottom: 60px;
+    bottom: 60px;
 }
 .ms-organization-content {
-  /*padding: 15px 10px;*/
+    /*padding: 15px 10px;*/
 }
 .ms-organization-options {
-  list-style: none;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  max-height: 160px;
+    list-style: none;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    max-height: 160px;
 }
 .ms-organization-options li {
-  padding: 10px;
-  cursor: pointer;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  color: rgba(10,18,32,.64)
+    padding: 10px;
+    cursor: pointer;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    color: rgba(10, 18, 32, 0.64);
 }
-.ms-organization-options li:hover{
-  color: rgba(10,18,32,.87);
-  background-color: rgba(232,235,237,.6)
+.ms-organization-options li:hover {
+    color: rgba(10, 18, 32, 0.87);
+    background-color: rgba(232, 235, 237, 0.6);
 }
-.ms-organization-options li.active{
-  color: rgba(81,130,228,1);
+.ms-organization-options li.active {
+    color: rgba(81, 130, 228, 1);
 }
 </style>
--- src/views/Content/Organization.vue
+++ src/views/Content/Organization.vue
@@ -27,152 +27,155 @@
 import MsFormOrganization from '@/components/Form/FormOrganization'
 import * as api from '@/api'
 export default {
-  data () {
-    return {
-      // 单选
-      selectedOne: '',
-      single: [],
-      // 多级联动
-      selectedMore1: [],
-      selectedMore2: [],
-      multiple: [],
-      structure: [],
-      currStructure: []
-    }
-  },
-  created () {
-    this.single = api.single()
-    this.multiple = api.multiple()
-    this.genOrganizaion()
-  },
-  methods: {
-    genOrganizaion () {
-      let data = [{
-        code: 420000,
-        name: '湖北省',
-        parentCode: 0
-      },
-      {
-        code: 420100,
-        name: '武汉市',
-        parentCode: 420000
-      },
-      {
-        code: 420101,
-        name: '市辖区',
-        parentCode: 420100
-      },
-      {
-        code: 420102,
-        name: '江岸区',
-        parentCode: 420100
-      },
-      {
-        code: 420103,
-        name: '江汉区',
-        parentCode: 420100
-      },
-      {
-        code: 420104,
-        name: '硚口区',
-        parentCode: 420100
-      },
-      {
-        code: 420105,
-        name: '汉阳区',
-        parentCode: 420100
-      },
-      {
-        code: 421000,
-        name: '荆州市',
-        parentCode: 420000
-      },
-      {
-        code: 421001,
-        name: '市辖区',
-        parentCode: 421000
-      },
-      {
-        code: 421002,
-        name: '沙市区',
-        parentCode: 421000
-      },
-      {
-        code: 421003,
-        name: '荆州区',
-        parentCode: 421000
-      },
-      {
-        code: 430000,
-        name: '湖南省',
-        parentCode: 0
-      },
-      {
-        code: 430100,
-        name: '长沙市',
-        parentCode: 430000
-      },
-      {
-        code: 430101,
-        name: '市辖区',
-        parentCode: 430100
-      },
-      {
-        code: 430102,
-        name: '芙蓉区',
-        parentCode: 430100
-      },
-      {
-        code: 430103,
-        name: '天心区',
-        parentCode: 430100
-      },
-      {
-        code: 430104,
-        name: '岳麓区',
-        parentCode: 430100
-      }]
-      for (let i = 0; i < data.length; i++) {
-        this.structure.push({
-          value: data[i].code,
-          parentValue: data[i].parentCode,
-          label: data[i].name,
-          nodeData: data[i]
-        })
-      }
-      this.currStructure = this.buildTree(this.structure)
-    },
-    buildTree (nodes) {
-      let len = nodes.length // 节点数量
-      let tops = [] // 顶级节点数组，如果只有一个顶级节点，该数组只有一个元素，并且该顶级节点是根节点。
-      if (len === 1) return nodes[0]
-      tops = nodes.filter((m) => {
-        if (!m.parentValue) {
-          return m
-        } else {
-          let hasParent = nodes.find((n) => {
-            if (n.value === m.parentValue) {
-              if (!n.children) {
-                n.children = []
-              }
-              n.children.push(m)
-              n.leaf = false
-              return n
-            }
-          })
-          if (!hasParent) {
-            return m
-          }
+    data() {
+        return {
+            // 单选
+            selectedOne: '',
+            single: [],
+            // 多级联动
+            selectedMore1: [],
+            selectedMore2: [],
+            multiple: [],
+            structure: [],
+            currStructure: []
         }
-      })
-      return tops
+    },
+    created() {
+        this.single = api.single()
+        this.multiple = api.multiple()
+        this.genOrganizaion()
+    },
+    methods: {
+        genOrganizaion() {
+            let data = [
+                {
+                    code: 420000,
+                    name: '湖北省',
+                    parentCode: 0
+                },
+                {
+                    code: 420100,
+                    name: '武汉市',
+                    parentCode: 420000
+                },
+                {
+                    code: 420101,
+                    name: '市辖区',
+                    parentCode: 420100
+                },
+                {
+                    code: 420102,
+                    name: '江岸区',
+                    parentCode: 420100
+                },
+                {
+                    code: 420103,
+                    name: '江汉区',
+                    parentCode: 420100
+                },
+                {
+                    code: 420104,
+                    name: '硚口区',
+                    parentCode: 420100
+                },
+                {
+                    code: 420105,
+                    name: '汉阳区',
+                    parentCode: 420100
+                },
+                {
+                    code: 421000,
+                    name: '荆州市',
+                    parentCode: 420000
+                },
+                {
+                    code: 421001,
+                    name: '市辖区',
+                    parentCode: 421000
+                },
+                {
+                    code: 421002,
+                    name: '沙市区',
+                    parentCode: 421000
+                },
+                {
+                    code: 421003,
+                    name: '荆州区',
+                    parentCode: 421000
+                },
+                {
+                    code: 430000,
+                    name: '湖南省',
+                    parentCode: 0
+                },
+                {
+                    code: 430100,
+                    name: '长沙市',
+                    parentCode: 430000
+                },
+                {
+                    code: 430101,
+                    name: '市辖区',
+                    parentCode: 430100
+                },
+                {
+                    code: 430102,
+                    name: '芙蓉区',
+                    parentCode: 430100
+                },
+                {
+                    code: 430103,
+                    name: '天心区',
+                    parentCode: 430100
+                },
+                {
+                    code: 430104,
+                    name: '岳麓区',
+                    parentCode: 430100
+                }
+            ]
+            for (let i = 0; i < data.length; i++) {
+                this.structure.push({
+                    value: data[i].code,
+                    parentValue: data[i].parentCode,
+                    label: data[i].name,
+                    nodeData: data[i]
+                })
+            }
+            this.currStructure = this.buildTree(this.structure)
+        },
+        buildTree(nodes) {
+            let len = nodes.length // 节点数量
+            let tops = [] // 顶级节点数组，如果只有一个顶级节点，该数组只有一个元素，并且该顶级节点是根节点。
+            if (len === 1) return nodes[0]
+            tops = nodes.filter(m => {
+                if (!m.parentValue) {
+                    return m
+                } else {
+                    let hasParent = nodes.find(n => {
+                        if (n.value === m.parentValue) {
+                            if (!n.children) {
+                                n.children = []
+                            }
+                            n.children.push(m)
+                            n.leaf = false
+                            return n
+                        }
+                    })
+                    if (!hasParent) {
+                        return m
+                    }
+                }
+            })
+            return tops
+        }
+    },
+    components: {
+        MsFormOrganization
     }
-  },
-  components: {
-    MsFormOrganization
-  }
 }
 </script>
 
 <style lang="less">
+
 </style>
